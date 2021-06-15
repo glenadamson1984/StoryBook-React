12e042a8fb6cef9763d2240fba72cfa6d2b8ab17
glenadamson1984 <glenadamson1984@gmail.com>
2021-06-15 16:48:07 +0100

    renaming and sorting stories in version 6

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,3 @@
-
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,5 +5,11 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+    options: {
+      storySort: (a, b) =>
+        a[1].kind === b[1].kind
+          ? 0
+          : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+    },
   },
-}
+};
--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -9,3 +9,5 @@ export default {
 export const Small = () => <Input size="small" placeholder="Small" />;
 export const Medium = () => <Input size="medium" placeholder="Medium" />;
 export const Large = () => <Input size="large" placeholder="Large" />;
+
+Small.storyName = "Small input";
