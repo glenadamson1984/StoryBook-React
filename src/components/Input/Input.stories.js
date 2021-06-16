20b157307d19bc2d27c6203c3889703e87a1a5b8
glenadamson1984 <glenadamson1984@gmail.com>
2021-06-16 08:53:00 +0100

    addons which are part on storybook such as control / actions / docs and viewport

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -4,10 +4,34 @@ import Input from "./Input";
 export default {
   title: "Input Controls/Text",
   component: Input,
+  // to add actions to the storybook page
+  argTypes: { onClick: { action: "Clicked" } },
 };
 
-export const Small = () => <Input size="small" placeholder="Small" />;
-export const Medium = () => <Input size="medium" placeholder="Medium" />;
-export const Large = () => <Input size="large" placeholder="Large" />;
+// export const Small = () => <Input size="small" placeholder="Small" />;
+// export const Medium = () => <Input size="medium" placeholder="Medium" />;
+// export const Large = () => <Input size="large" placeholder="Large" />;
+
+// changing from this format above to the one below allows for the controls section at the bottom of the
+// storybook page to be enabled so you can now switch between modes using the controls
+const Template = (args) => <Input {...args} />;
+
+export const Small = Template.bind({});
+Small.args = {
+  size: "small",
+  placeholder: "Small",
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  size: "medium",
+  placeholder: "Medium",
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: "large",
+  placeholder: "Large",
+};
 
 Small.storyName = "Small input";
